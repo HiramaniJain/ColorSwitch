452149144e34ba23222aba261965b8faf1a19c4f
HIRAMANI JAIN <51274748+HiramaniJain@users.noreply.github.com>
2019-06-01 16:15:33 -0700

    Add files via upload

---
+++ ColorSwitch/ViewController/GameViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GameViewController.swift
+//  ColorSwitch
+//
+//  Created by Heeral on 4/22/19.
+//  Copyright © 2019 heeral. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            
+            let scene =  GameScene( size: view.bounds.size)
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+}
+
---
+++ ColorSwitch/template/Settings.swift
@@ -0,0 +1,15 @@
+//
+//  Settings.swift
+//  ColorSwitch
+//
+//  Created by Heeral on 4/23/19.
+//  Copyright © 2019 heeral. All rights reserved.
+//
+
+import SpriteKit
+
+enum PhysicsCategories{
+    static let none: UInt32 = 0
+    static let ballCategory: UInt32 = 0x1
+    static let SwitchCategory: UInt32 = 0x1 << 1
+}
